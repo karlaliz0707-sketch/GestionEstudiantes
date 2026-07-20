4dafce7420f2fbd53a0b0f5af1be30c76b2c11a3
karlaliz0707-sketch <karlaliz0707@gmail.com>
2026-07-20 11:11:51 -0600

    Update GestionEstudiantes.java

--- GestionEstudiantes.java
+++ GestionEstudiantes.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.gestionestudiantes;
-
-/**
- *
- * @author rogelio
- */
 public class GestionEstudiantes {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Actividad 2 - Gestion de Estudiantes");
-    System.out.println("Nombre:Karla");// karla
-    System.out.println("Matricula: 12345678"); // 3342062
+        // Actividad 3 - Prueba de Herencia 
+
+    // Polimorfismo: objetos  de clases derivadas
+    Estudiante e1 = new EstudianteBecado(1, "Alejandra Lopez", "IPC-610", 50);
+        Estudiante e2 = new EstudianteGraduado(2, "Maria Celestino", "SMT", "Cert-2024");
+
+    System.out.printIn("--- Probando Herencia ---");
+        e1.mostrarInfo(); // Llamada al metodo sobreescrito
+    System.out.printIn();
+        e2.mostrarInfo(); // Llamada al metodo sobreescrito
+
+    System.out.printIn("\n---Metodos exclusivos ---");
+        ((EstudianteBecado) e1).aplicarBeca();
+        ((EstudianteGraduado) e2).generarCerticado();
     }
 }
-
